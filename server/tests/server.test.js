7d7a01865f25ef154b6e35ea8c8bb61fe3ee07cd
Eijei26 <tongcoeijei@gmail.com>
2019-01-09 11:53:00 +0800

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -109,3 +109,45 @@ describe('GET /todos:id', () => {
     });
 
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        let hexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            }).end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+
+                // Todo.findById(hexId).then((todo) => {
+                //     expect(todo).toNotExist();
+                //     done();
+                // }).catch((e) => done(e));
+
+            });
+    });
+
+    it('should return a 404, if todo is not found', (done) => {
+
+        let hexId = new ObjectID().toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return a 404 for non-object id', (done) => {
+
+        request(app)
+            .delete(`/todos/1212342dsfs`)
+            .expect(404)
+            .end(done);
+    });
+
+});
