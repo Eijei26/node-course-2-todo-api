df3a40c780dc0b3777381b5c66adebb4911375c8
Eijei26 <tongcoeijei@gmail.com>
2019-01-10 15:30:46 +0800

    Add POST /users/login route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,6 +3,7 @@ const request = require('supertest');
 const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
 const _ = require('lodash');
 
 const todos = [
@@ -193,4 +194,22 @@ describe('PATCH /todos/:id', () => {
             })
             .end(done);
     });
+});
+
+describe('GET /users/me', () => {
+    it('should return user if authenticated', (done) => {
+        request(app)
+            .get('/users/me')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body._id).toBe(users[0]._id.toHexString());
+                expect(res.body.email).toBe(users[0].email);
+            })
+            .end(done);
+    });
+
+    // it('should return 401 if not authenticated', (done) => {
+
+    // });
 });
